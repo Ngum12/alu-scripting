0c8ef2ec96992866b1027dbff168bf04aaddb46b
Ngum12 <d.ngum@alustudent.com>
2023-11-12 22:54:11 -0800

    ajusted 3

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -17,7 +17,7 @@ def count_words(subreddit, word_list, after='', hot_list=[]):
     if request.status_code == 200:
         data = request.json()
 
-        for topic in (data['data']['children']):
+        for topic in data['data']['children']:
             for word in topic['data']['title'].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
@@ -25,27 +25,23 @@ def count_words(subreddit, word_list, after='', hot_list=[]):
 
         after = data['data']['after']
         if after is None:
-            save = []
             for i in range(len(word_list)):
                 for j in range(i + 1, len(word_list)):
                     if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
                         hot_list[i] += hot_list[j]
+                        hot_list[j] = 0
 
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (hot_list[j] > hot_list[i] or
-                            (word_list[i] > word_list[j] and
-                             hot_list[j] == hot_list[i])):
-                        a = hot_list[i]
-                        hot_list[i] = hot_list[j]
-                        hot_list[j] = a
-                        a = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = a
+            sorted_words = sorted(zip(word_list, hot_list), key=lambda x: (-x[1], x[0]))
 
-            for i in range(len(word_list)):
-                if (hot_list[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), hot_list[i]))
+            for word, count in sorted_words:
+                if count > 0:
+                    print(f"{word.lower()}: {count}")
         else:
             count_words(subreddit, word_list, after, hot_list)
+
+
+# Example usage
+subreddit_name = "unpopular"
+keywords_list = ['you', 'unpopular', 'vote', 'down', 'downvote', 'her', 'politics']
+count_words(subreddit_name, keywords_list)
+
