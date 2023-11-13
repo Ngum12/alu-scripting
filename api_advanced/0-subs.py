dcc92e885c9c8d054beaabb305fab9f0e174fbb0
Ngum12 <d.ngum@alustudent.com>
2023-11-12 21:45:46 -0800

    api advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Writing a function that queries the Reddit API and returns
-the number of subscribers for a given subreddit."""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        subscriber_number = json_data.get('data').get(
-            'children')[0].get('data').get('subreddit_subscribers')
-        return subscriber_number
-    else:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
